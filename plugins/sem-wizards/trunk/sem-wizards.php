d546b419a99c3280080faefdeeb8ce55c01ccf2c
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-27 11:42:32 +0000

    #147: new Sem Cloner plugin
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@328 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- plugins/sem-wizards/trunk/sem-wizards.php
+++ plugins/sem-wizards/trunk/sem-wizards.php
@@ -1,31 +1,34 @@
 <?php
-/*
-Plugin Name: Semiologic Wizards
-Plugin URI: http://www.getsemiologic.com
-Description: Semiologic Pro Wizards
-Author: Denis de Bernardy
-Version: 1.5 alpha
-Author URI: http://www.getsemiologic.com
-Update Service: http://version.semiologic.com/plugins
-Update Tag: sem_wizards
-*/
+// obsolete file
 
-/*
-Terms of use
-------------
+$active_plugins = get_option('active_plugins');
 
-This software is copyright Mesoconcepts and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+if ( !is_array($active_plugins) )
+{
+	$active_plugins = array();
+}
 
-http://www.mesoconcepts.com/license/
-**/
+foreach ( (array) $active_plugins as $key => $plugin )
+{
+	if ( $plugin == 'sem-wizards/sem-wizards.php' )
+	{
+		unset($active_plugins[$key]);
+		break;
+	}
+}
+
+if ( !in_array('sem-cloner/sem-cloner.php', $active_plugins) )
+{
+	$active_plugins[] = 'sem-cloner/sem-cloner.php';
+}
+
+if ( !in_array('version-checker/version-checker.php', $active_plugins) )
+{
+	$active_plugins[] = 'version-checker/version-checker.php';
+}
 
 
-define('sem_wizards_path', dirname(__FILE__));
+sort($active_plugins);
 
-global $sem_wizards_files;
-global $sem_wizards_admin_files;
-
-$sem_wizards_files = array('rpc.php');
-$sem_wizards_admin_files = array('wizards.php');
-
+update_option('active_plugins', $active_plugins);
 ?>
