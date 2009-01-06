7c1c9031177e2ddb54cf086a364893e05ed03f21
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-06 11:50:43 +0000

    Tag sem-docs 2.0
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@505 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/sem-docs/tags/2.0/sem-docs.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Semiologic Documentation
+Plugin URI: http://www.semiologic.com/software/wp-tweaks/sem-docs/
+Description: Semiologic Pro Documentation
+Author: Denis de Bernardy
+Version: 2.0
+Author URI: http://www.getsemiologic.com
+Update Service: http://version.semiologic.com/plugins
+Update Tag: sem_docs
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+
+http://www.mesoconcepts.com/license/
+**/
+
+
+define('sem_docs_path', dirname(__FILE__));
+
+global $sem_docs_files;
+global $sem_docs_admin_files;
+
+$sem_docs_files = array();
+$sem_docs_admin_files = array('docs.php');
+?>
