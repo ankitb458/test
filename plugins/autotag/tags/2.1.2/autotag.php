6544fd0f155230943a8af7a86c0e535a5a72b80f
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-26 12:58:53 +0000

    Tag autotag 2.1.2
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@680 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/autotag/tags/2.1.2/autotag.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: AutoTag
+Plugin URI: http://www.semiologic.com/software/publishing/autotag/
+Description: Leverages Yahoo!'s term extraction web service to automatically tag your posts.
+Version: 2.1.2
+Author: Denis de Bernardy
+Author URI: http://www.getsemiologic.com
+Update Package: https://members.semiologic.com/media/plugins/autotag/autotag.zip
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts (http://www.mesoconcepts.com), and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+
+http://www.mesoconcepts.com/license/
+**/
+
+
+if ( is_admin() )
+{
+	include dirname(__FILE__) . '/autotag-admin.php';
+}
+?>
