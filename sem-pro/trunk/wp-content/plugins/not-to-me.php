b1780b2fb847244e0841a7d1f8e35b85c159ba3a
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-08 18:26:47 +0000

    drop not to me
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@1254 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-pro/trunk/wp-content/plugins/not-to-me.php
+++ sem-pro/trunk/wp-content/plugins/not-to-me.php
@@ -1,19 +1,3 @@
 <?php
-/*
-(c) Scott Merrill <http://www.skippy.net/>
-GPL Licensed
-*/
-
-add_action('comment_post', 'not_to_me');
-
-function not_to_me($ID = 0) {
-global $cache_settings;
-
-$comment = get_commentdata($ID, 1, 1);
-if (get_settings('admin_email') == $comment['comment_author_email']) {
-$foo = get_settings('comments_notify'); // make sure it's cached
-$cache_settings->comments_notify = 0;
-}
-return $ID;
-}
+// obsolete file
 ?>
