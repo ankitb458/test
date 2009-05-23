74a80ac0ce4f24f2d0cad852a55b14a451a88fb4
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-23 21:27:12 +0000

    kill sem-docs
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@1469 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ sem-pro/trunk/wp-content/plugins/sem-docs/sem-docs.php
@@ -0,0 +1,23 @@
+<?php
+// obsolete file
+
+$active_plugins = get_option('active_plugins');
+
+if ( !is_array($active_plugins) )
+{
+	$active_plugins = array();
+}
+
+foreach ( (array) $active_plugins as $key => $plugin )
+{
+	if ( $plugin == 'sem-docs/sem-docs.php' )
+	{
+		unset($active_plugins[$key]);
+		break;
+	}
+}
+
+sort($active_plugins);
+
+update_option('active_plugins', $active_plugins);
+?>
