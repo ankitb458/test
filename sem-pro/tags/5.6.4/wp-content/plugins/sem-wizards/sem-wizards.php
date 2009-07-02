312755ee1a1bfed663609094714e35d0066ae2a5
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-02 19:50:01 +0000

    next tag
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@1941 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ sem-pro/tags/5.6.4/wp-content/plugins/sem-wizards/sem-wizards.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Semiologic Wizards
+Plugin URI: http://www.getsemiologic.com
+Description: Semiologic Pro Wizards
+Author: Denis de Bernardy
+Version: 1.4
+Author URI: http://www.semiologic.com
+Update Service: http://version.semiologic.com/wordpress
+Update Tag: sem_wizards
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+
+http://www.mesoconcepts.com/license/
+**/
+
+
+define('sem_wizards_path', dirname(__FILE__));
+
+global $sem_wizards_files;
+global $sem_wizards_admin_files;
+
+$sem_wizards_files = array('rpc.php');
+$sem_wizards_admin_files = array('wizards.php');
+
+?>
