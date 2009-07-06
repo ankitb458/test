8fdd4bddd0beff3ff76e1e36f39fdce0f63b60b1
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-06 16:57:30 +0000

    continuing on widget upgrade scripts
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@1974 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-pro/trunk/wp-content/plugins/sem-recent-links/sem-recent-links.php
+++ sem-pro/trunk/wp-content/plugins/sem-recent-links/sem-recent-links.php
@@ -1,25 +1,18 @@
 <?php
 // obsolete file
+$active_plugins = get_option('active_plugins', array());
 
-$active_plugins = get_option('active_plugins');
-
-if ( !is_array($active_plugins) )
-{
-	$active_plugins = array();
-}
-
-foreach ( (array) $active_plugins as $key => $plugin )
-{
-	if ( $plugin == 'sem-recent-links/sem-recent-links.php' )
-	{
+foreach ( (array) $active_plugins as $key => $plugin ) {
+	if ( $plugin == 'sem-recent-links/sem-recent-links.php' ) {
 		unset($active_plugins[$key]);
 		break;
 	}
 }
 
-if ( !in_array('fuzzy-widgets/fuzzy-widgets.php', $active_plugins) )
-{
+if ( !in_array('fuzzy-widgets/fuzzy-widgets.php', $active_plugins) ) {
 	$active_plugins[] = 'fuzzy-widgets/fuzzy-widgets.php';
+	include_once WP_PLUGIN_DIR . '/fuzzy-widgets/fuzzy-widgets.php';
+	fuzzy_widget::activate();
 }
 
 sort($active_plugins);
