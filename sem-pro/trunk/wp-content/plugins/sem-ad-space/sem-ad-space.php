9469ad0d184a9dab535f3b015ad67666f267ce90
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-07 18:43:08 +0000

    ad spaces upgrader + misc other upgrade fixes + permission fixes
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@1985 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-pro/trunk/wp-content/plugins/sem-ad-space/sem-ad-space.php
+++ sem-pro/trunk/wp-content/plugins/sem-ad-space/sem-ad-space.php
@@ -1,28 +1,48 @@
 <?php
 # obsolete file
-if ( get_option('sem_ad_space_params') ) :
+if ( !get_option('sem_ad_space_params') ) :
 
 $active_plugins = get_option('active_plugins');
 
-if ( !in_array('ad-manager/ad-manager.php', $active_plugins) )
-{
-	$active_plugins[] = 'ad-manager/ad-manager.php';
-	include_once WP_PLUGIN_DIR . '/ad-manager/ad-manager.php';
-}
-
-if ( !in_array('inline-widgets/inline-widgets.php', $active_plugins) )
-{
-	$active_plugins[] = 'inline-widgets/inline-widgets.php';
-	include_once WP_PLUGIN_DIR . '/inline-widgets/inline-widgets.php';
+foreach ( (array) $active_plugins as $key => $plugin ) {
+	if ( $plugin == 'sem-ad-space/sem-ad-space.php' ) {
+		unset($active_plugins[$key]);
+		break;
+	}
 }
 
 sort($active_plugins);
 
 update_option('active_plugins', $active_plugins);
 
+elseif ( !defined('DOING_CRON') ) :
 
-function export_ad_spaces()
-{
+$active_plugins = get_option('active_plugins');
+
+if ( !in_array('ad-manager/ad-manager.php', $active_plugins) ) {
+	$new_plugin = 'ad-manager/ad-manager.php';
+	$active_plugins[] = $new_plugin;
+	include_once WP_PLUGIN_DIR . '/' . $new_plugin;
+	do_action('activate_' . $new_plugin);
+}
+
+if ( !in_array('inline-widgets/inline-widgets.php', $active_plugins) ) {
+	$new_plugin = 'inline-widgets/inline-widgets.php';
+	$active_plugins[] = $new_plugin;
+	include_once WP_PLUGIN_DIR . '/' . $new_plugin;
+	do_action('activate_' . $new_plugin);
+}
+
+sort($active_plugins);
+
+update_option('active_plugins', $active_plugins);
+
+function export_ad_spaces() {
+	if ( !get_option('sem_ad_space_params') )
+		return;
+	
+	$ignore_user_abort = ignore_user_abort(true);
+	
 	global $wpdb;
 	global $table_prefix;
 	global $wp_ozh_wsa;
@@ -35,50 +55,8 @@ function export_ad_spaces()
 	$wpdb->ad_distribution2post = $table_prefix . "ad_distribution2post";
 
 	$options = get_option('sem_ad_space_params');
+	delete_option('sem_ad_space_params');
 	
-	function ad_spaces_export_inline_ad($input)
-	{
-		static $widget_ids = array();
-		
-		global $wpdb;
-		
-		$ad_name = trim($input[1]);
-		
-		$ad_block = $wpdb->get_row("
-			SELECT	*
-			FROM	$wpdb->ad_blocks
-			WHERE	ad_block_name = '" . $wpdb->escape($ad_name) . "'
-			");
-			
-		if ( !$ad_block ) return '';
-		
-		if ( isset($widget_ids[$ad_name]) )
-		{
-			$widget_id = $widget_ids[$ad_name];
-		}
-		else
-		{
-			if ( strpos($ad_block->ad_block_code, 'src=\\"') !== false )
-			{
-				$ad_block->ad_block_code = stripslashes($ad_block->ad_block_code);
-			}
-			
-			$widget_id = ad_manager::new_widget(array(
-				'title' => trim($ad_block->ad_block_name),
-				'code' => $ad_block->ad_block_code,
-				));
-			
-			$widget_ids[$ad_name] = $widget_id;
-
-			$sidebars_widgets = get_option('sidebars_widgets');
-			$sidebars_widgets['inline_widgets'][] = $widget_id;
-			update_option('sidebars_widgets', $sidebars_widgets);
-		}
-			
-		return '[widget:' . $widget_id . ']';
-	}
-
-
 	# convert inline ads in posts
 
 	$default_ad = $options['default_ad_block_name'];
@@ -89,8 +67,7 @@ function export_ad_spaces()
 		WHERE	posts.post_content REGEXP '<!--ad[^>]+-->'
 		");
 	
-	foreach ( $posts as $post )
-	{
+	foreach ( $posts as $post ) {
 		#dump(esc_html($post->post_content));
 
 		$post->post_content = preg_replace("/
@@ -149,8 +126,7 @@ function export_ad_spaces()
 	
 	
 	# migrate location-specific ads
-	foreach ( $options['default_ad_block'] as $area => $ad_block_id )
-	{
+	foreach ( $options['default_ad_block'] as $area => $ad_block_id ) {
 		if ( $area == 'top' ) continue;
 		if ( $ad_block_id <= 0 ) continue;
 
@@ -160,22 +136,39 @@ function export_ad_spaces()
 			WHERE	ad_block_id = " . intval($ad_block_id) . "
 			");
 
-		if ( !$ad_block ) return '';
+		if ( !$ad_block )
+			return '';
 
-		if ( strpos($ad_block->ad_block_code, 'src=\\"') !== false )
-		{
+		if ( strpos($ad_block->ad_block_code, 'src=\\"') !== false ) {
 			$ad_block->ad_block_code = stripslashes($ad_block->ad_block_code);
 		}
 		
-		$widget_id = ad_manager::new_widget(array(
+		$ops = array(
 			'title' => trim($ad_block->ad_block_name),
 			'code' => $ad_block->ad_block_code,
-			));
+			);
 		
-		$sidebars_widgets = get_option('sidebars_widgets');
-
-		switch ( $area )
-		{
+		$ad_widgets = get_option('widget_ad_unit', array());
+		unset($ad_widgets['_multiwidget']);
+		if ( !$ad_widgets )
+			$widget_id = 2;
+		else
+			$widget_id = max(array_keys($ad_widgets)) + 1;
+		$ad_widgets[$widget_id] = $ops;
+		update_option('widget_ad_unit', $ad_widgets);
+		
+		$widget_id = "ad_unit-$widget_id";
+		
+		if ( is_admin() ) {
+			$sidebars_widgets = get_option('sidebars_widgets', array('array_version' => 3));
+		} else {
+			global $_wp_sidebars_widgets;
+			if ( !$_wp_sidebars_widgets )
+				$_wp_sidebars_widgets = get_option('sidebars_widgets', array('array_version' => 3));
+			$sidebars_widgets =& $_wp_sidebars_widgets;
+		}
+		
+		switch ( $area ) {
 		case 'header':
 			$sidebars_widgets['the_header'][] = $widget_id;
 			break;
@@ -193,16 +186,15 @@ function export_ad_spaces()
 			array_unshift($sidebars_widgets['the_footer'], $widget_id);
 			break;
 		case 'sidebar':
-			if ( isset($sidebars_widgets['sidebar-1']) )
-			{
+			if ( isset($sidebars_widgets['sidebar-1']) ) {
 				$sidebars_widgets['sidebar-1'][] = $widget_id;
-			}
-			elseif ( isset($sidebars_widgets['sidebar-ext']) )
-			{
-				$sidebars_widgets['sidebar-ext'][] = $widget_id;
+			} elseif ( isset($sidebars_widgets['sidebar-2']) ) {
+				$sidebars_widgets['sidebar-2'][] = $widget_id;
+			} elseif ( isset($sidebars_widgets['ext_sidebar']) ) {
+				$sidebars_widgets['ext_sidebar'][] = $widget_id;
 			}
 		}
-
+		
 		update_option('sidebars_widgets', $sidebars_widgets);
 	}
 	
@@ -215,34 +207,76 @@ function export_ad_spaces()
 		'ad_distributions',
 		'ad_distribution2tag',
 		'ad_distribution2post',
-		) as $table )
-	{
+		) as $table ) {
 		$wpdb->query("DROP TABLE IF EXISTS `" . $wpdb->{$table} . "`");
 	}
 
 	$wpdb->query("DELETE FROM $wpdb->postmeta WHERE meta_key = '_ad_distribution'");
 
-	delete_option('sem_ad_space_params');
-
-
-	# uninstall plugin
-
-	$active_plugins = get_option('active_plugins');
-	$key = array_search('sem-ad-space/sem-ad-space.php', $active_plugins);
-	unset($active_plugins[$key]);
-	sort($active_plugins);
-	update_option('active_plugins', $active_plugins);
+	global $wp_widget_factory;
+	if ( method_exists($wp_widget_factory->widgets['ad_manager'], '_register') )
+		$wp_widget_factory->widgets['ad_manager']->_register();
+	
+	ignore_user_abort($ignore_user_abort);
 } # export_ad_spaces()
 
-else :
+function ad_spaces_export_inline_ad($input) {
+	static $widget_ids = array();
+	
+	global $wpdb;
+	
+	$ad_name = trim($input[1]);
+	
+	$ad_block = $wpdb->get_row("
+		SELECT	*
+		FROM	$wpdb->ad_blocks
+		WHERE	ad_block_name = '" . $wpdb->escape($ad_name) . "'
+		");
+		
+	if ( !$ad_block )
+		return '';
+	
+	if ( isset($widget_ids[$ad_name]) ) {
+		$widget_id = $widget_ids[$ad_name];
+	} else {
+		if ( strpos($ad_block->ad_block_code, 'src=\\"') !== false ) {
+			$ad_block->ad_block_code = stripslashes($ad_block->ad_block_code);
+		}
+		
+		$ops = array(
+			'title' => trim($ad_block->ad_block_name),
+			'code' => $ad_block->ad_block_code,
+			);
+		
+		$ad_widgets = get_option('widget_ad_unit', array());
+		unset($ad_widgets['_multiwidget']);
+		if ( !$ad_widgets )
+			$widget_id = 2;
+		else
+			$widget_id = max(array_keys($ad_widgets)) + 1;
+		$ad_widgets[$widget_id] = $ops;
+		update_option('widget_ad_unit', $ad_widgets);
+		
+		$widget_id = "ad_unit-$widget_id";
+		
+		$widget_ids[$ad_name] = $widget_id;
+		
+		if ( is_admin() ) {
+			$sidebars_widgets = get_option('sidebars_widgets', array('array_version' => 3));
+		} else {
+			global $_wp_sidebars_widgets;
+			if ( !$_wp_sidebars_widgets )
+				$_wp_sidebars_widgets = get_option('sidebars_widgets', array('array_version' => 3));
+			$sidebars_widgets =& $_wp_sidebars_widgets;
+		}
+		$sidebars_widgets['inline_widgets'][] = $widget_id;
+		update_option('sidebars_widgets', $sidebars_widgets);
+	}
+		
+	return '[widget id="' . $widget_id . '"/]';
+}
 
-# uninstall plugin
-
-$active_plugins = get_option('active_plugins');
-$key = array_search('sem-ad-space/sem-ad-space.php', $active_plugins);
-unset($active_plugins[$key]);
-sort($active_plugins);
-update_option('active_plugins', $active_plugins);
+add_action('init', 'export_ad_spaces', 3000);
 
 endif;
 ?>
