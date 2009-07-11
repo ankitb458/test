8a6be367248f451c49fd2212822a6bc1ac861195
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-11 20:34:04 +0000

    democracy 1.16RC2
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@2065 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-pro/trunk/wp-content/plugins/democracy/democracy.php
+++ sem-pro/trunk/wp-content/plugins/democracy/democracy.php
@@ -3,7 +3,7 @@
 Plugin Name: Poll Widget / Democracy
 Plugin URI: http://blog.jalenack.com/archives/democracy/
 Description: Ajax polling plugin
-Version: 1.15.1 RC fork
+Version: 1.16 RC2 fork
 Author: Andrew Sutherland
 Author URI: http://blog.jalenack.com/
 */
