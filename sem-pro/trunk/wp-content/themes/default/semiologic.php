3e1b2a3811b16bf8eaf253783b6b2f6d67a3408a
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-12 12:03:30 +0000

    make sure sem-reloaded exists before firing the automated install
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@2080 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-pro/trunk/wp-content/themes/default/semiologic.php
+++ sem-pro/trunk/wp-content/themes/default/semiologic.php
@@ -1,7 +1,7 @@
 <?php
 #delete_option('sem6_options');
 
-if ( !get_option('sem6_options') ) :
+if ( !get_option('sem6_options') && file_exists(WP_CONTENT_DIR . '/themes/sem-reloaded/inc/init.php') ) :
 
 delete_option('current_theme');
 update_option('template', 'sem-reloaded');
