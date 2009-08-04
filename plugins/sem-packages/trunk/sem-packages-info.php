6cd5f78b1a49a0ef11ac17d00cbd44c3e484b6b3
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-08-04 11:54:59 +0000

    sem-packages v.1.0
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@2363 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/sem-packages/trunk/sem-packages-info.php
@@ -0,0 +1,4 @@
+<?php
+__('Semiologic Packages', 'sem-packages');
+__('Interfaces with Mediacaster in order to output extra information for stable and bleeding edge downloads.', 'sem-packages');
+?>
