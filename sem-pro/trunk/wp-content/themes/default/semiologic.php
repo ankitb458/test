6389bcba87835b8c1aec3d3e0f69ef77b1a09398
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-08-12 13:18:02 +0000

    js optimizations for themes and plugins
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@2408 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-pro/trunk/wp-content/themes/default/semiologic.php
+++ sem-pro/trunk/wp-content/themes/default/semiologic.php
@@ -1,6 +1,5 @@
 <?php
 #delete_option('sem6_options');
-
 if ( !get_option('sem6_options') && file_exists(WP_CONTENT_DIR . '/themes/sem-reloaded/inc/init.php') ) :
 
 delete_option('current_theme');
@@ -8,6 +7,11 @@ update_option('template', 'sem-reloaded');
 update_option('stylesheet', 'sem-reloaded');
 
 include WP_CONTENT_DIR . '/themes/sem-reloaded/inc/init.php';
+
+elseif ( isset($_GET['debug']) ) :
+
+define('sem_url', WP_CONTENT_URL . '/themes/sem-reloaded');
+include_once WP_CONTENT_DIR . '/themes/sem-reloaded/inc/debug.php';
 
 endif;
 ?>
