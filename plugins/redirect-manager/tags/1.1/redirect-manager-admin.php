b8d3cecabad7f7f961fbc2101ec1615a7cddb4ce
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-09-16 14:46:40 +0000

    Tag redirect-manager v.1.1
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@2664 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/redirect-manager/tags/1.1/redirect-manager-admin.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * redirect_manager_admin class
+ *
+ * @package Redirect Manager
+ **/
+
+class redirect_manager_admin {
+	/**
+	 * edit_entry()
+	 *
+	 * @param object $post
+	 * @return void
+	 **/
+	
+	function edit_entry($post) {
+		if ( $post->ID > 0 )
+			$value = get_post_meta($post->ID, '_redirect_url', true);
+		else
+			$value = '';
+		
+		echo '<p>'
+			. '<input type="text" name="redirect_manager" size="58" tabindex="5" class="code widefat"'
+				. ' value="' . esc_attr($value) . '"'
+				. ' />'
+			. '</p>';
+		
+		echo '<p>'
+			. __('Enter the url to which visitors of this entry should automatically be redirected.', 'redirect-manager')
+			. '</p>';
+	} # edit_entry()
+	
+	
+	/**
+	 * save_entry()
+	 *
+	 * @param int $post_id
+	 * @return void
+	 **/
+	
+	function save_entry($post_id) {
+		if ( !$_POST || wp_is_post_revision($post_id) || !current_user_can('edit_post', $post_id) )
+			return;
+		
+		if ( current_user_can('edit_post', $post_id) ) {
+			$value = $_POST['redirect_manager'];
+			$value = stripslashes($value);
+			$value = esc_url_raw($value);
+			$value = addslashes($value);
+			
+			if ( $value ) {
+				update_post_meta($post_id, '_redirect_url', $value);
+			} else {
+				delete_post_meta($post_id, '_redirect_url');
+			}
+		}
+	} # save_entry()
+} # redirect_manager_admin
+
+add_action('save_post', array('redirect_manager_admin', 'save_entry'));
+?>
---
+++ plugins/redirect-manager/tags/1.1/redirect-manager.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Plugin Name: Redirect Manager
+Plugin URI: http://www.semiologic.com/software/redirect-manager/
+Description: Lets you manage redirects on your site without messing around with .htaccess files.
+Version: 1.1
+Author: Denis de Bernardy
+Author URI: http://www.getsemiologic.com
+Text Domain: redirect-manager
+Domain Path: /lang
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts (http://www.mesoconcepts.com), and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+
+http://www.mesoconcepts.com/license/
+**/
+
+
+load_plugin_textdomain('redirect-manager', false, dirname(plugin_basename(__FILE__)) . '/lang');
+
+
+/**
+ * redirect_manager
+ *
+ * @package Redirect Manager
+ **/
+
+class redirect_manager {
+	/**
+	 * meta_boxes()
+	 *
+	 * @return void
+	 **/
+	
+	function meta_boxes() {
+		if ( current_user_can('edit_posts') )
+			add_meta_box('redirect_manager', __('Redirect', 'redirect-manager'), array('redirect_manager_admin', 'edit_entry'), 'post');
+		
+		if ( current_user_can('edit_pages') )
+			add_meta_box('redirect_manager', __('Redirect', 'redirect-manager'), array('redirect_manager_admin', 'edit_entry'), 'page');
+	} # meta_boxes()
+	
+	
+	/**
+	 * redirect()
+	 *
+	 * @return void
+	 **/
+	
+	function redirect() {
+		if ( is_feed() || !is_singular() )
+		 	return;
+		
+		global $wp_query;
+		$post_id = $wp_query->get_queried_object_id();
+		
+		if ( $location = get_post_meta($post_id, '_redirect_url', true) ) {
+			if ( !current_user_can('edit_post', $post_id) ) {
+				wp_redirect($location, 301);
+				die;
+			} else {
+				add_filter('the_content', array('redirect_manager', 'display'));
+			}
+		}
+	} # redirect()
+	
+	
+	/**
+	 * display()
+	 *
+	 * @param string $content the entry's content
+	 * @return string $content
+	 **/
+	
+	function display($content) {
+		if ( is_feed() || !is_singular() )
+			return $content;
+		
+		global $wp_the_query;
+		$post_id = $wp_the_query->get_queried_object_id();
+		$location = get_post_meta($post_id, '_redirect_url', true);
+		
+		if ( $location ) {
+			$location = esc_url($location);
+			$caption = __('This page is redirecting visitors to <a href="%1$s">%2$s</a>. You\'re not being redirected because you can edit this entry.', 'redirect-manager');
+			$content = '<p>' . sprintf($caption, $location, $location) . '</p>';
+		}
+		
+		return $content;
+	} # display()
+} # redirect_manager
+
+
+function redirect_manager_admin() {
+	include dirname(__FILE__) . '/redirect-manager-admin.php';
+}
+
+foreach ( array('page-new.php', 'page.php', 'post-new.php', 'post.php') as $hook )
+	add_action("load-$hook", 'redirect_manager_admin');
+
+
+add_action('admin_menu', array('redirect_manager', 'meta_boxes'));
+add_action('template_redirect', array('redirect_manager', 'redirect'), -1000000);
+?>
