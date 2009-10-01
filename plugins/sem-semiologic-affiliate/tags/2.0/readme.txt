e7dd7f6fc119b7ce1ec166dd4722a9d9efffa192
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-10-01 09:51:16 +0000

    Tag sem-semiologic-affiliate v.2.0
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@2871 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/sem-semiologic-affiliate/tags/2.0/readme.txt
@@ -0,0 +1,42 @@
+=== Semiologic Affiliate ===
+Contributors: Denis-de-Bernardy
+Donate link: http://www.semiologic.com/partners/
+Tags: affiliate, marketing, internet-marketing, semiologic
+Requires at least: 2.8
+Tested up to: 2.8.4
+Stable tag: trunk
+
+Automatically adds your affiliate ID to all links to Semiologic.
+
+
+== Description ==
+
+The Semiologic Affiliate plugin will add your [Semiologic affiliate ID](http://www.semiologic.com/partners/) to links on your site that point to [semiologic.com](http://www.semiologic.com) or [getsemiologic.com](http://www.getsemiologic.com).
+
+Start by signing up with the [Semiologic Affiliate Program](http://www.semiologic.com/partners/), and creating an affiliate campaign.
+
+Then, configure the plugin under Settings / Semiologic Affiliate, by entering your affiliate campaign.
+
+From that point onwards, any link to semiologic.com or getsemiologic.com will now have that ID attached to it.
+
+= Help Me! =
+
+The [Semiologic forum](http://forum.semiologic.com) is the best place to report issues.
+
+Alternatively, email sales at semiologic dot com.
+
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Change Log ==
+
+= 2.0 =
+
+- New, better regular expression for link processing
+- Enhance escape/unescape methods
+- Localization
+- Code enhancements and optimizations
---
+++ plugins/sem-semiologic-affiliate/tags/2.0/sem-semiologic-affiliate.php
@@ -0,0 +1,129 @@
+<?php
+/*
+Plugin Name: Semiologic Affiliate
+Plugin URI: http://www.semiologic.com/software/sem-affiliate/
+Description: Automatically adds your affiliate ID to all links to Semiologic.
+Version: 2.0
+Author: Denis de Bernardy
+Author URI: http://www.getsemiologic.com
+Text Domain: sem-semiologic-affiliate
+Domain Path: /lang
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts (http://www.mesoconcepts.com), and is distributed under the terms of the GPL license, v.2.
+
+http://www.opensource.org/licenses/gpl-2.0.php
+**/
+
+
+load_plugin_textdomain('sem-semiologic-affiliate', false, dirname(plugin_basename(__FILE__)) . '/lang');
+
+if ( !defined('semiologic_affiliate_debug') )
+	define('semiologic_affiliate_debug', false);
+
+
+/**
+ * semiologic_affiliate
+ *
+ * @package Semiologic Affiliate
+ **/
+
+class semiologic_affiliate {
+	/**
+	 * admin_menu()
+	 *
+	 * @return void
+	 **/
+
+	function admin_menu() {
+		add_options_page(
+			__('Semiologic Affiliate', 'sem-semiologic-affiliate'),
+			__('Semiologic Affiliate', 'sem-semiologic-affiliate'),
+			'manage_options',
+			'semiologic_affiliate',
+			array('semiologic_affiliate_admin', 'edit_options')
+			);
+	} # admin_menu()
+	
+	
+	/**
+	 * get_campaign()
+	 *
+	 * @return string $aff_id
+	 **/
+
+	function get_campaign() {
+		$o = get_option('sem_campaign_key');
+		
+		if ( $o === false )
+			$o = semiologic_affiliate::init_campaign();
+		
+		return $o;	
+	} # get_campaign()
+	
+	
+	/**
+	 * init_campaign()
+	 *
+	 * @return string $aff_id
+	 **/
+
+	function init_campaign() {
+		$o = get_option('sem_semiologic_affiliate_params');
+		
+		if ( $o !== false ) {
+			$o = isset($options['aff_id']) ? trim($options['aff_id']) : '';
+			delete_option('sem_semiologic_affiliate_params');
+		} else {
+			$o = '';
+		}
+		
+		update_option('sem_campaign_key', $o);
+		
+		return $o;
+	} # init_campaign()
+	
+	
+	/**
+	 * filter()
+	 *
+	 * @param array $anchor
+	 * @return array $anchor
+	 **/
+
+	function filter($anchor) {
+		if ( !preg_match("/^https?:\/\/(?:www\.)?(?:get)?semiologic\.com\b/", $anchor['attr']['href']) )
+			return $anchor;
+		
+		if ( strpos($anchor['attr']['href'], '?') === false )
+			$anchor['attr']['href'] .= '?aff=' . urlencode(semiologic_affiliate::get_campaign());
+		
+		return $anchor;
+	} # filter()
+} # semiologic_affiliate
+
+
+function semiologic_affiliate_admin() {
+	include dirname(__FILE__) . '/sem-semiologic-affiliate-admin.php';
+}
+
+add_action('load-settings_page_semiologic_affiliate', 'semiologic_affiliate_admin');
+
+
+add_action('admin_menu', array('semiologic_affiliate', 'admin_menu'));
+
+if ( !is_admin() && semiologic_affiliate::get_campaign() ) {
+	if ( !class_exists('anchor_utils') )
+		include dirname(__FILE__) . '/anchor-utils/anchor-utils.php';
+	
+	if ( !semiologic_affiliate_debug ) {
+		add_filter('ob_filter_anchor', array('semiologic_affiliate', 'filter'));
+	} else {
+		add_filter('filter_anchor', array('semiologic_affiliate', 'filter'));
+	}
+}
+?>
