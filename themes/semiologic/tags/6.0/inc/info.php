27d8dbbbe86fba3ec7ab8456ba52d78c1d3292d8
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-10-03 17:05:08 +0000

    untag sem theme 6.0
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@2941 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- themes/semiologic/tags/6.0/inc/info.php
+++
@@ -1,9 +0,0 @@
-<?php
-__('Semiologic', 'sem-theme');
-__('The Semiologic theme (obsolete).', 'sem-theme');
-__('Archives', 'sem-theme');
-__('Links', 'sem-theme');
-__('Sales Letter', 'sem-theme');
-__('Mono Column', 'sem-theme');
-__('Special Page', 'sem-theme');
-?>
