7de4a7b70258f59cca6970fcb7687f45cbb8a5da
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-10-08 09:52:39 +0000

    untag sem theme
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@3002 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- themes/semiologic/tags/6.0/sell-page.php
+++
@@ -1,8 +0,0 @@
-<?php
-# obsolete file
-
-delete_post_meta($GLOBALS['wp_the_query']->get_queried_object_id(), '_wp_page_template');
-add_post_meta($GLOBALS['wp_the_query']->get_queried_object_id(), '_wp_page_template', 'letter.php', true);
-
-include sem_path . '/letter.php';
-?>
