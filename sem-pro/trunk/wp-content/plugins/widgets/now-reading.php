1b7232af9977cdab42e51584180acb85ed400287
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-11-29 18:59:49 +0000

    drop obsolete plugins
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@3673 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-pro/trunk/wp-content/plugins/widgets/now-reading.php
+++
@@ -1,1 +0,0 @@
-<?php $active_plugins = get_option('active_plugins', array()); foreach ( $active_plugins as $key => $plugin ) { if ( $plugin == 'widgets/now-reading.php' ) { unset($active_plugins[$key]); sort($active_plugins); update_option('active_plugins', $active_plugins); break; } } ?>
