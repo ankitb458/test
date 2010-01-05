b15c842c77cb19fe39c6bd4db74cce25223c5fec
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-01-05 10:43:57 +0000

    Tag inline-widgets v.2.0.1
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@3883 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/inline-widgets/tags/2.0.1/tinymce/langs/en.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n("en.inline_widgets",{
+	widgets : "Widgets",
+	whats_this : "What's this?",
+	whats_this_tip : "This drop down lets you insert inline widgets you've configured under Appearance / Widgets."
+});
