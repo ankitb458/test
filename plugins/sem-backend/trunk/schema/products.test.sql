6345d80cccbf5445c3d9cd7f99a8282de9316ac2
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-03-25 20:20:48 +0000

    more tests
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4096 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- plugins/sem-backend/trunk/schema/products.test.sql
+++ plugins/sem-backend/trunk/schema/products.test.sql
@@ -1,13 +1,13 @@
-INSERT INTO products ( status, init_price )
-VALUES	( 'active', 60 );
+INSERT INTO products
+DEFAULT VALUES;
 
 UPDATE	products
 SET		status = 'trash';
 
 DELETE FROM products;
 
-INSERT INTO products ( init_price )
-VALUES	( 60 );
+INSERT INTO products ( init_price, init_comm, rec_price, rec_comm )
+VALUES	( 12, 6, 12, 6 );
 
 UPDATE	products
 SET		status = 'active';
@@ -15,4 +15,54 @@ SET		status = 'active';
 UPDATE	products
 SET		status = 'trash';
 
-SELECT	campaigns.status = 'inherit' AS check_campaign_inherit FROM campaigns;
+SELECT	campaigns.status = 'inherit' AS check_campaign_inherit
+FROM	campaigns;
+
+UPDATE	campaigns
+SET		init_discount = 12,
+		rec_discount = 12;
+
+SELECT	init_discount = 6 AND rec_discount = 6 as check_autofix_promos_discount
+FROM	campaigns
+WHERE	aff_id IS NULL;
+
+INSERT INTO users (email) VALUES ('foo@bar.com');
+
+-- UPDATE	campaigns SET aff_id = users.id FROM users; -- must fail
+
+INSERT INTO campaigns(aff_id, init_discount, rec_discount) SELECT id, 12, 12 FROM users;
+
+SELECT	init_discount = 0 AND
+		rec_discount = 0
+		as check_campaigns_autofix_campaign_discount
+FROM	campaigns
+WHERE	aff_id IS NOT NULL;
+
+UPDATE	campaigns
+SET		init_discount = 12,
+		rec_discount = 12,
+		product_id = products.id
+FROM	products
+WHERE	campaigns.aff_id IS NOT NULL;
+
+SELECT	init_discount = 0 AND
+		rec_discount = 0
+		as check_campaigns_autofix_invalid_coupon_discount
+FROM	campaigns
+WHERE	aff_id IS NOT NULL;
+
+UPDATE	products
+SET		status = 'active';
+
+UPDATE	campaigns
+SET		init_discount = 12,
+		rec_discount = 12,
+		product_id = products.id
+FROM	products
+WHERE	campaigns.aff_id IS NOT NULL;
+
+SELECT	init_discount = 6 AND
+		rec_discount = 6
+		as check_campaigns_autofix_valid_coupon_discount
+FROM	campaigns
+WHERE	aff_id IS NOT NULL;
--- plugins/sem-backend/trunk/schema/test.sql
+++ plugins/sem-backend/trunk/schema/test.sql
@@ -4,36 +4,4 @@ BEGIN;
 
 \i ./products.test.sql
 
-INSERT INTO products ( init_price )
-VALUES	( 60 );
-
-UPDATE	products
-SET		status = 'active';
-
-UPDATE	campaigns
-SET		status = 'active',
-		init_discount = 30,
-		min_date = now() - interval '3 days',
-		max_date = now() + interval '1 day',
-		max_orders = 3,
-		firesale = true;
-
-INSERT INTO campaigns
-DEFAULT VALUES;
-
-INSERT INTO orders
-DEFAULT VALUES;
-
-INSERT INTO order_lines ( order_id, product_id )
-SELECT	orders.id,
-		products.id
-FROM	orders,
-		products;
-
 COMMIT;
-
---SELECT * FROM products;
---SELECT * FROM campaigns;
---SELECT * FROM orders;
-SELECT * FROM order_lines;
-
