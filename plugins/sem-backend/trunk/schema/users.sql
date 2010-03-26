344a7b1d0b0a06cbb0128fab563343f10b04dfd6
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-03-26 13:34:28 +0000

    add a constraint on emails
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4103 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- plugins/sem-backend/trunk/schema/users.sql
+++ plugins/sem-backend/trunk/schema/users.sql
@@ -5,8 +5,27 @@ CREATE TABLE users (
 	id				bigserial PRIMARY KEY,
 	uuid			uuid NOT NULL DEFAULT uuid() UNIQUE,
 	name			varchar(255) NOT NULL DEFAULT '',
-	email			varchar(255) NOT NULL UNIQUE
+	email			varchar(255) NOT NULL UNIQUE,
+	CONSTRAINT valid_email
+		CHECK ( email ~ '^[0-9a-z_-]+(?:\.[0-9a-z_-]+)*@(?:[0-9a-z_-]+\.)+[0-9a-z]+$' )
 );
 
 CREATE INDEX users_sort ON users(name);
 CREATE INDEX users_email ON users(email);
+
+/**
+ * Cleans a user before storing it
+ */
+CREATE OR REPLACE FUNCTION users_clean()
+	RETURNS trigger
+AS $$
+BEGIN
+	NEW.name := trim(NEW.name);
+	NEW.email := trim(lower(NEW.email));
+	 
+	RETURN NEW;
+END $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER users_3_clean
+	BEFORE INSERT OR UPDATE ON users
+FOR EACH ROW EXECUTE PROCEDURE users_clean();
