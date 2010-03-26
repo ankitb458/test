9e53e9407941f85e029c8e2d533d3f83f4bfcdc9
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-03-26 20:45:34 +0000

    run tests as a function
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4113 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- plugins/sem-backend/trunk/schema/products.test.sql
+++ plugins/sem-backend/trunk/schema/products.test.sql
@@ -1,82 +1,122 @@
-INSERT INTO products
-DEFAULT VALUES;
+/**
+ * Tests products
+ */
+CREATE OR REPLACE FUNCTION test_products()
+	RETURNS VOID
+AS $$
+DECLARE
+	res			boolean;
+BEGIN
+	INSERT INTO products
+	DEFAULT VALUES;
 
-UPDATE	products
-SET		status = 'trash';
+	UPDATE	products
+	SET		status = 'trash';
 
-DELETE FROM products;
+	DELETE FROM products;
 
-INSERT INTO products ( init_price, init_comm, rec_price, rec_comm )
-VALUES	( 12, 6, 12, 6 );
+	INSERT INTO products ( init_price, init_comm, rec_price, rec_comm )
+	VALUES	( 12, 6, 12, 6 );
 
-UPDATE	products
-SET		status = 'active';
+	UPDATE	products
+	SET		status = 'active';
 
-UPDATE	products
-SET		status = 'trash';
+	UPDATE	products
+	SET		status = 'trash';
+	
+	SELECT	campaigns.status = 'inherit'
+	INTO	res
+	FROM	campaigns;
+	
+	IF	res IS NOT TRUE
+	THEN
+		RAISE WARNING 'Failed: inherit campaign status';
+	END IF;
 
-SELECT	campaigns.status = 'inherit' AS check_campaign_inherit
-FROM	campaigns;
+	UPDATE	campaigns
+	SET		init_discount = 12,
+			rec_discount = 12;
+	
+	SELECT init_discount = 6 AND
+			rec_discount = 6
+	INTO	res
+	FROM	campaigns
+	WHERE	aff_id IS NULL;
+	
+	IF	res IS NOT TRUE
+	THEN
+		RAISE WARNING 'Failed: autofix promos discount';
+	END IF;
 
-UPDATE	campaigns
-SET		init_discount = 12,
-		rec_discount = 12;
+	INSERT INTO users ( email ) VALUES ( 'foo@bar.com' );
 
-SELECT	init_discount = 6 AND rec_discount = 6 as check_autofix_promos_discount
-FROM	campaigns
-WHERE	aff_id IS NULL;
+	-- UPDATE	campaigns SET aff_id = users.id FROM users; -- must fail
 
-INSERT INTO users ( email ) VALUES ( 'foo@bar.com' );
+	INSERT INTO campaigns ( aff_id, init_discount, rec_discount ) SELECT id, 12, 12 FROM users;
+	
+	SELECT	init_discount = 0 AND
+			rec_discount = 0
+	INTO	res
+	FROM	campaigns
+	WHERE	aff_id IS NOT NULL;
+	
+	IF	res IS NOT TRUE
+	THEN
+		RAISE WARNING	'Failed: autofix campaign discount';
+	END IF;
+	
+	UPDATE	campaigns
+	SET		init_discount = 12,
+			rec_discount = 12,
+			product_id = products.id
+	FROM	products
+	WHERE	campaigns.aff_id IS NOT NULL;
+	
+	SELECT	init_discount = 0 AND
+			rec_discount = 0
+	INTO	res
+	FROM	campaigns
+	WHERE	aff_id IS NOT NULL;
+	
+	IF	res IS NOT TRUE
+	THEN
+		RAISE WARNING 'Failed: autofix invalid coupon discount';
+	END IF;
 
--- UPDATE	campaigns SET aff_id = users.id FROM users; -- must fail
+	UPDATE	products
+	SET		status = 'active';
+	
+	UPDATE	campaigns
+	SET		init_discount = 12,
+			rec_discount = 12,
+			product_id = products.id
+	FROM	products
+	WHERE	campaigns.aff_id IS NOT NULL;
 
-INSERT INTO campaigns ( aff_id, init_discount, rec_discount ) SELECT id, 12, 12 FROM users;
+	SELECT	init_discount = 6 AND
+			rec_discount = 6
+	INTO	res
+	FROM	campaigns
+	WHERE	aff_id IS NOT NULL;
+	
+	IF	res IS NOT TRUE
+	THEN
+		RAISE WARNING 'Failed: autofix valid coupon discount';
+	END IF;
 
-SELECT	init_discount = 0 AND
-		rec_discount = 0
-		as check_campaigns_autofix_campaign_discount
-FROM	campaigns
-WHERE	aff_id IS NOT NULL;
+	-- clean up
 
-UPDATE	campaigns
-SET		init_discount = 12,
-		rec_discount = 12,
-		product_id = products.id
-FROM	products
-WHERE	campaigns.aff_id IS NOT NULL;
+	UPDATE	products
+	SET		status = 'trash';
 
-SELECT	init_discount = 0 AND
-		rec_discount = 0
-		as check_campaigns_autofix_invalid_coupon_discount
-FROM	campaigns
-WHERE	aff_id IS NOT NULL;
+	DELETE FROM products;
 
-UPDATE	products
-SET		status = 'active';
+	UPDATE	campaigns
+	SET		status = 'trash';
 
-UPDATE	campaigns
-SET		init_discount = 12,
-		rec_discount = 12,
-		product_id = products.id
-FROM	products
-WHERE	campaigns.aff_id IS NOT NULL;
+	DELETE FROM campaigns;
 
-SELECT	init_discount = 6 AND
-		rec_discount = 6
-		as check_campaigns_autofix_valid_coupon_discount
-FROM	campaigns
-WHERE	aff_id IS NOT NULL;
+	DELETE FROM users;
+END $$ LANGUAGE plpgsql;
 
--- clean up
-
-UPDATE	products
-SET		status = 'trash';
-
-DELETE FROM products;
-
-UPDATE	campaigns
-SET		status = 'trash';
-
-DELETE FROM campaigns;
-
-DELETE FROM users;
+DROP FUNCTION test_products();
