5e1e70252829a914b9ae443ab1d3f3995443fa13
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-04-02 12:54:18 +0000

    move fields around
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4280 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- plugins/sem-backend/trunk/schema/invoice-lines.schema.sql
+++ plugins/sem-backend/trunk/schema/invoice-lines.schema.sql
@@ -5,19 +5,17 @@ CREATE TABLE invoice_lines (
 	id				bigserial PRIMARY KEY,
 	uuid			uuid NOT NULL DEFAULT uuid() UNIQUE,
 	status			status_payable NOT NULL DEFAULT 'draft',
+	name			varchar NOT NULL,
 	due				datetime,
 	cleared			datetime,
-	name			varchar NOT NULL,
-	tx_id			bigint NOT NULL REFERENCES invoices(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	invoice_id		bigint NOT NULL REFERENCES invoices(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	parent_id		bigint REFERENCES invoice_lines(id) ON UPDATE CASCADE,
 	order_line_id	bigint REFERENCES order_lines(id) ON UPDATE CASCADE,
 	user_id			bigint REFERENCES users(id) ON UPDATE CASCADE,
 	amount			numeric(8,2) NOT NULL,
 	shipping		numeric(8,2) NOT NULL,
-	fee				numeric(8,2) NOT NULL,
 	tax				numeric(8,2) NOT NULL,
-	ext_tx_id		varchar(128) UNIQUE,
-	ext_status		varchar(64) NOT NULL DEFAULT '',
+	fee				numeric(8,2) NOT NULL,
 	created			datetime NOT NULL DEFAULT NOW(),
 	modified		datetime NOT NULL DEFAULT NOW(),
 	memo			text NOT NULL DEFAULT '',
@@ -38,7 +36,7 @@ SELECT	timestampable('invoice_lines'),
 		trashable('invoice_lines');
 
 CREATE INDEX invoice_lines_sort ON invoice_lines(cleared DESC);
-CREATE INDEX invoice_lines_tx_id ON invoice_lines(tx_id);
+CREATE INDEX invoice_lines_invoice_id ON invoice_lines(invoice_id);
 CREATE INDEX invoice_lines_order_line_id ON invoice_lines(order_line_id);
 CREATE INDEX invoice_lines_user_id ON invoice_lines(user_id);
 
