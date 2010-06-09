317dd3f699e2703a2898d20cac729b8b703b5ad0
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-06-09 13:46:14 +0000

    Tag sem-semiologic-affiliate v.2.1
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4459 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/sem-semiologic-affiliate/tags/2.1/readme.txt
@@ -0,0 +1,50 @@
+=== Semiologic Affiliate ===
+Contributors: Denis-de-Bernardy
+Donate link: http://www.semiologic.com/partners/
+Tags: affiliate, marketing, internet-marketing, semiologic
+Requires at least: 2.8
+Tested up to: 3.0
+Stable tag: trunk
+
+Automatically adds your affiliate ID to all links to Semiologic.
+
+
+== Description ==
+
+The Semiologic Affiliate plugin will add your [Semiologic affiliate ID](http://www.semiologic.com/partners/) to links on your site that point to [semiologic.com](http://www.semiologic.com) or [getsemiologic.com](http://www.getsemiologic.com).
+
+Start by signing up with the [Semiologic Affiliate Program](http://www.semiologic.com/partners/), and creating an affiliate campaign.
+
+Then, configure the plugin under Settings / Semiologic Affiliate, by entering your affiliate campaign.
+
+From that point onwards, any link to semiologic.com or getsemiologic.com will now have that ID attached to it.
+
+= Help Me! =
+
+The [Semiologic forum](http://forum.semiologic.com) is the best place to report issues.
+
+Alternatively, email sales at semiologic dot com.
+
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Change Log ==
+
+= 2.1 =
+
+- WP 3.0 compat
+
+= 2.0.1 =
+
+- Force a higher pcre.backtrack_limit and pcre.recursion_limit to avoid blank screens on large posts
+
+= 2.0 =
+
+- New, better regular expression for link processing
+- Enhance escape/unescape methods
+- Localization
+- Code enhancements and optimizations
---
+++ plugins/sem-semiologic-affiliate/tags/2.1/sem-semiologic-affiliate-admin.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * semiologic_affiliate_admin
+ *
+ * @package Semiologic Affiliate
+ **/
+
+class semiologic_affiliate_admin {
+	/**
+	 * save_options()
+	 *
+	 * @return void
+	 **/
+
+	function save_options() {
+		if ( !$_POST || !current_user_can('manage_options') )
+			return;
+		
+		if ( function_exists('is_super_admin') && !is_super_admin() )
+			return;
+		
+		check_admin_referer('semiologic_affiliate');
+		
+		$campaign_key = stripslashes($_POST['campaign_key']);
+		
+		if ( preg_match("/\?aff=(.+)/i", $campaign_key, $match) )
+			$campaign_key = end($match);
+		
+		$campaign_key = trim(preg_replace("/[^0-9a-zA-Z_-]/", "", $campaign_key));
+		
+		update_site_option('sem_campaign_key', $campaign_key);
+		
+		echo "<div class=\"updated fade\">\n"
+			. "<p>"
+				. "<strong>"
+				. __('Settings saved.', 'sem-semiologic-affiliate')
+				. "</strong>"
+			. "</p>\n"
+			. "</div>\n";
+	} # save_options()
+	
+	
+	/**
+	 * edit_options()
+	 *
+	 * @return void
+	 **/
+
+	function edit_options() {
+		if ( function_exists('is_super_admin') && !is_super_admin() )
+			return;
+		
+		echo '<div class="wrap">' . "\n";
+		
+		echo '<form method="post" action="">' . "\n";
+		
+		wp_nonce_field('semiologic_affiliate');
+		
+		screen_icon();
+		
+		echo '<h2>' . __('Semiologic Affiliate Settings', 'sem-semiologic-affiliate') . '</h2>' . "\n";
+		
+		$aff_id = semiologic_affiliate::get_campaign();
+		
+		echo '<table class="form-table">' . "\n";
+		
+		echo '<p>'
+			. __('Fill in the field that follows to make all links to semiologic.com and getsemiologic.com on the site use your campaign ID automatically.', 'sem-semiologic-affiliate')
+			. '</p>' . "\n";
+
+		echo '<tr>' . "\n"
+			. '<th scope="row">'
+			. '<label for="campaign_key">'
+			. sprintf(__('Your <a href="%s">Campaign ID</a>', 'sem-semiologic-affiliate'), 'http://oldbackend.semiologic.com')
+			. '</label>'
+			. '</th>' . "\n"
+			. '<td>'
+			. '<label for="campaign_key">http://www.getsemiologic.com?aff=</label>'
+			. '<input type="text"'
+				. ' name="campaign_key" id="campaign_key"'
+				. ' value="' . esc_attr($aff_id) . '"'
+				. ' />'
+			. '</td>'
+			. '</tr>' . "\n";
+		
+		echo '</table>' . "\n";
+		
+		echo '<div class="submit">'
+			. '<input type="submit" value="' . esc_attr(__('Save Changes', 'sem-semiologic-affiliate')) . '" />'
+			. '</div>' . "\n";
+		
+		echo '</form>' . "\n";
+		
+		echo '</div>' . "\n";
+	} # edit_options()
+} # semiologic_affiliate_admin
+
+add_action('settings_page_semiologic_affiliate', array('semiologic_affiliate_admin', 'save_options'), 0);
+?>
