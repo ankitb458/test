e31220bb3e2dc5c92f139782442ee16508b35493
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-06-09 13:31:12 +0000

    Tag pc-robots-txt v.1.3 fork
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4439 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/pc-robots-txt/tags/1.3/pc-robots-txt.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: PC Robots.txt
+Plugin URI: http://petercoughlin.com/wp-plugins/
+Description: Create and manage a virtual robots.txt file for your blog.
+Version: 1.3 fork
+Author: Peter Coughlin
+Author URI: http://petercoughlin.com/
+*/
+
+
+function pc_robots_txt() {
+
+	if ( strpos($_SERVER['REQUEST_URI'], '/robots.txt') !== false ) {
+		$options = get_site_option('pc_robots_txt');
+		
+		if ( !is_array($options) || !$options['user_agents'] )
+			$options = pc_robots_txt_set_defaults();
+		
+		$pc_robots_txt = stripslashes($options['user_agents']);
+		
+		header( 'Content-Type: text/plain; charset=utf-8' );
+		echo trim($pc_robots_txt) . "\n";
+		
+		do_action( 'do_robotstxt' );
+	}# end if ( strpos($_SERVER['REQUEST_URI'], '/robots.txt') !== false ) {
+
+}# end function pc_robots_txt()
+
+
+function pc_robots_txt_set_defaults() {
+	$options = get_option('pc_robots_txt');
+	
+	if ( !$options ) {
+		$options = array(
+			"user_agents" => "User-agent: *\n"
+			. "Disallow: /wp-*\n"
+			. "Allow: /wp-content/uploads\n"
+			);
+	}
+	
+	update_site_option('pc_robots_txt', $options);
+
+	return $options;
+
+}# end function pc_robots_txt_set_defaults() {
+
+
+function pc_robots_txt_init() {
+
+	if ( get_option('blog_public') == '1' ) {
+		remove_action('do_robots', 'do_robots');
+		add_action('do_robots', 'pc_robots_txt', -1000);
+	}
+
+	if ( is_admin() )
+		include_once dirname(__FILE__) . '/pc-robots-txt-admin.php';
+
+}# end function pc_robots_txt_init() {
+
+add_action('init', 'pc_robots_txt_init');
+?>
---
+++ plugins/pc-robots-txt/tags/1.3/readme.txt
@@ -0,0 +1,24 @@
+=== PC Robots.txt ===
+Contributors: Peter Coughlin, Denis de Bernardy
+Tags: semiologic
+Requires at least: 2.8
+Tested up to: 3.0
+Stable tag: trunk
+
+A robots.txt file manager.
+
+
+== Description ==
+
+A robots.txt file manager.
+
+
+== Change Log ==
+
+= 1.3 =
+
+- WP 3.0 compat
+
+= 1.2 =
+
+- Don't interfere with canonical redirects
