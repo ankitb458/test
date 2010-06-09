3502e41ec2a2114be51cc529ce5fa60c0550f782
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-06-09 13:18:55 +0000

    bump
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4427 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- plugins/democracy/trunk/democracy.php
+++ plugins/democracy/trunk/democracy.php
@@ -3,7 +3,7 @@
 Plugin Name: Poll Widget / Democracy
 Plugin URI: http://blog.jalenack.com/archives/democracy/
 Description: Ajax polling plugin
-Version: 1.16.3 RC fork
+Version: 1.16.3 fork
 Author: Andrew Sutherland
 Author URI: http://blog.jalenack.com/
 */
