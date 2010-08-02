883d914f2d83a5bbd376e62436402916d57aa187
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-08-02 14:25:16 +0000

    Tag sem-cache v.2.1.1
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4513 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/sem-cache/tags/2.1.1/cache-fs.php
@@ -0,0 +1,220 @@
+<?php
+/**
+ * cache_fs
+ *
+ * @package Semiologic Cache
+ **/
+
+class cache_fs {
+	private static $base_dir;
+	
+	
+	/**
+	 * start()
+	 *
+	 * @return void
+	 **/
+
+	static function start() {
+		if ( strpos($_SERVER['HTTP_HOST'], '/') !== false )
+			trigger_error(sprintf('%s is not a valid hostname', $_SERVER['HTTP_HOST']), E_USER_ERROR);
+		
+		self::$base_dir = defined('SUBDOMAIN_INSTALL') && SUBDOMAIN_INSTALL
+			? WP_CONTENT_DIR . '/cache/' . $_SERVER['HTTP_HOST']
+			: WP_CONTENT_DIR . '/cache';
+	} # start()
+	
+	
+	/**
+	 * path_join()
+	 *
+	 * @param string $file
+	 * @return string $file
+	 **/
+
+	static protected function path_join($file) {
+		return rtrim(self::$base_dir . '/' . ltrim($file, '/'), '/');
+	} # path_join()
+	
+	
+	/**
+	 * exists()
+	 *
+	 * @param string $file
+	 * @param int $timeout
+	 * @return bool $valid
+	 **/
+
+	static function exists($file, $timeout = false) {
+		$file = self::path_join($file);
+		
+		return file_exists($file)
+			&& ( !$timeout || ( filemtime($file) + $timeout >= time() ) );
+	} # exists()
+	
+	
+	/**
+	 * get_contents()
+	 *
+	 * @param string $file
+	 * @return string $contents
+	 **/
+
+	static function get_contents($file) {
+		$file = self::path_join($file);
+		
+		return file_get_contents($file);
+	} # get_contents()
+	
+	
+	/**
+	 * readfile()
+	 *
+	 * @param string $file
+	 * @return int $bytes
+	 **/
+
+	static function readfile($file) {
+		$file = self::path_join($file);
+		
+		return readfile($file);
+	} # readfile()
+	
+	
+	/**
+	 * put_contents()
+	 *
+	 * @return bool $success
+	 **/
+
+	static function put_contents($file, $contents) {
+		$file = self::path_join($file);
+		
+		# bail if the file was concurrently built
+		if ( self::exists($file, 300) )
+			return true;
+		
+		$dir = dirname($file);
+		
+		if ( !wp_mkdir_p($dir) )
+			return false;
+		
+		$perms = stat($dir);
+		$perms = $perms['mode'] & 0000666;
+		
+		# work on a temporary file to bypass file locking requirements
+		$tmp = tempnam($dir, 'new_');
+		if ( !file_put_contents($tmp, $contents) ) {
+			unlink($tmp);
+			return false;
+		}
+		
+		if ( !chmod($tmp, $perms) || !rename($tmp, $file) ) {
+			unlink($tmp);
+			return false;
+		}
+		
+		return true;
+	} # put_contents()
+	
+	
+	/**
+	 * flush()
+	 *
+	 * @param string $dir
+	 * @param int $timeout
+	 * @param bool $recursive
+	 * @return bool $success
+	 **/
+	
+	static function flush($dir = '/', $timeout = false, $recursive = true) {
+		if ( function_exists('is_multisite') && is_multisite() )
+			return false;
+		
+		return self::rm(self::path_join($dir), $timeout, $recursive);
+	} # flush()
+	
+	
+	/**
+	 * rm()
+	 *
+	 * @param string $dir
+	 * @param int $timeout
+	 * @param bool $recursive
+	 * @return bool $success
+	 **/
+
+	static protected function rm($dir, $timeout = false, $recursive = true) {
+		$dir = rtrim($dir, '/');
+		
+		if ( !file_exists($dir) )
+			return true;
+		
+		if ( !is_dir($dir) )
+			return ( $timeout && ( filemtime($dir) + $timeout >= time() ) ) || unlink($dir);
+		elseif ( !$recursive )
+			return is_file("$dir/index.html") ? self::rm("$dir/index.html", $timeout, $recursive) : true;
+		
+		if ( !( $handle = opendir($dir) ) )
+			return false;
+		
+		$success = true;
+		while ( ( $file = readdir($handle) ) !== false ) {
+			if ( in_array($file, array('.', '..')) )
+				continue;
+			$success &= self::rm("$dir/$file", $timeout, $recursive);
+		}
+		
+		closedir($handle);
+		
+		return $timeout || rmdir($dir) && $success;
+	} # rm()
+	
+	
+	/**
+	 * stats()
+	 *
+	 * @param string $dir
+	 * @return array($total_pages, $expired_pages)
+	 **/
+
+	static function stats($dir = '/', $timeout = false, $bucket = null) {
+		static $total_pages = 0;
+		static $expired_pages = 0;
+		if ( !isset($bucket) ) {
+			$bucket = $dir;
+			$total_pages = 0;
+			$expired_pages = 0;
+			$dir = self::path_join($bucket);
+		}
+		
+		# asset files never expire
+		if ( $dir == self::$base_dir . '/assets' )
+			$timeout = false;
+		
+		if ( !file_exists($dir) ) {
+			return array($total_pages, $expired_pages);
+		} elseif ( is_file($dir) ) {
+			$total_pages++;
+			if ( $timeout && ( filemtime($dir) + $timeout < time() ) )
+				$expired_pages++;
+			return array($total_pages, $expired_pages);
+		} else {
+			if ( !( $handle = opendir($dir) ) )
+				return array($total_pages, $expired_pages);
+
+			while ( ( $file = readdir($handle) ) !== false ) {
+				if ( in_array($file, array('.', '..')) )
+					continue;
+				self::stats("$dir/$file", $timeout, $bucket);
+			}
+
+			closedir($handle);
+			
+			return array($total_pages, $expired_pages);
+		}
+	} # stats()
+} # cache_fs
+
+cache_fs::start();
+?>
---
+++ plugins/sem-cache/tags/2.1.1/readme.txt
@@ -0,0 +1,66 @@
+=== Semiologic Cache ===
+Contributors: Denis-de-Bernardy
+Donate link: http://www.semiologic.com/partners/
+Tags: semiologic
+Requires at least: 2.8
+Tested up to: 3.0
+Stable tag: trunk
+
+A high performance cache for WordPress.
+
+
+== Description ==
+
+The Semiologic Cache plugin for WordPress is a high performance cache for sites built using WP.
+
+Activate the Semiologic Cache plugin, and browse Settings / Cache to use the plugin.
+
+Contrary to similar WP plugins, this one strives for simplicity. There are no complicated looking options to choose from. The cache's various elements are either on, or off -- with no options.
+
+It implements each and every one of the following:
+
+- Static-level caching, which potentially serves pre-generated pages even before PHP loads
+- Query-level caching, which serves cached SQL queries to avoid hits to the database for logged in users
+- Persistent object-level caching, which makes WP objects persistent from a page to the next to avoid further hits to the database
+- Asset-level caching, which concatenates javascript and CSS files on the site's front end to avoid further hits to the server
+- GZip-level caching, which conditionally serves compressed files at the apache level (which is faster than using php)
+
+
+= Help Me! =
+
+The [Semiologic forum](http://forum.semiologic.com) is the best place to report issues.
+
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Change Log ==
+
+= 2.1.1 =
+
+- WP 3.0.1 compat
+
+= 2.1 =
+
+- WP 3.0 compat
+
+= 2.0.1 =
+
+- Fix Apache 1.3 quirks
+- Improve safe_mode and open_basedir handling
+
+= 2.0 =
+
+- Complete rewrite
+- Add query cache, object cache, asset cache and gzip cache
+
+= 1.2.1 =
+
+- Improve handling of custom wp-content and plugin folders
+
+= 1.2 =
+
+- Don't super cache requests with a cookie or GET parameter
