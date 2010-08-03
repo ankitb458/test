914dee4587fa4e79660673ea76089e881777bfb9
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-08-03 14:02:54 +0000

    movements - first batch of files
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4530 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/sem-backend/trunk/schema/movements.schema.sql
@@ -0,0 +1,71 @@
+/*
+ * Movements
+ */
+CREATE TABLE movements (
+	id				bigserial PRIMARY KEY,
+	uuid			uuid NOT NULL DEFAULT uuid() UNIQUE,
+	status			status_payable NOT NULL DEFAULT 'draft',
+	name			varchar NOT NULL,
+	movement_type	type_movement NOT NULL DEFAULT 'shipment',
+	movement_ref	varchar UNIQUE,
+	order_id		bigint REFERENCES orders(id),
+	user_id			bigint REFERENCES users(id),
+	issue_date		datetime NOT NULL DEFAULT NOW(),
+	due_date		datetime,
+	due_amount		numeric(8,2) NOT NULL DEFAULT 0,
+	cleared_date	datetime,
+	cleared_amount	numeric(8,2) NOT NULL DEFAULT 0,
+	created_date	datetime NOT NULL DEFAULT NOW(),
+	modified_date	datetime NOT NULL DEFAULT NOW(),
+	memo			text NOT NULL DEFAULT '',
+--	tsv				tsvector NOT NULL,
+	CONSTRAINT valid_name
+		CHECK ( name <> '' AND name = trim(name) ),
+	CONSTRAINT valid_flow
+		CHECK ( NOT ( issue_date IS NULL AND status > 'draft' ) AND
+			NOT ( due_date IS NULL AND status > 'draft' ) AND
+			NOT ( cleared_date IS NULL AND status = 'cleared' ) ),
+	CONSTRAINT valid_movement_type
+		CHECK ( movement_type = 'shipment' OR order_id IS NULL ),
+	CONSTRAINT valid_payment_method
+		CHECK ( payment_ref IS NULL OR payment_method IS NOT NULL ),
+	CONSTRAINT valid_payment_ref
+		CHECK ( payment_ref <> '' AND payment_ref = trim(payment_ref) ),
+	CONSTRAINT valid_amounts
+		CHECK ( due_amount >= 0 AND cleared_amount >= 0 )
+);
+
+SELECT	timestampable('movements'),
+		payable('movements'),
+--		searchable('movements'),
+		trashable('movements');
+
+CREATE INDEX movements_sort ON movements(due_date DESC);
+CREATE INDEX movements_user_id ON movements(order_id, user_id, due_date DESC);
+CREATE INDEX movements_order_id ON movements(order_id, due_date DESC);
+
+COMMENT ON TABLE movements IS E'Movements
+
+- due and cleared dates have absolutely no relationship with one another.
+  It is possible to advance deliver, late deliver, etc.';
+
+/**
+ * Process read-only fields
+ */
+CREATE OR REPLACE FUNCTION movements_readonly()
+	RETURNS trigger
+AS $$
+BEGIN
+	IF	ROW(NEW.id, NEW.movement_type, NEW.order_id)
+		IS DISTINCT FROM
+		ROW(OLD.id, OLD.movement_type, OLD.order_id)
+	THEN
+		RAISE EXCEPTION 'Can''t edit readonly field in movements.id = %', NEW.id;
+	END IF;
+	
+	RETURN NEW;
+END $$ LANGUAGE plpgsql;
+
+CREATE CONSTRAINT TRIGGER movements_01_readonly
+	AFTER UPDATE ON movements
+FOR EACH ROW EXECUTE PROCEDURE movements_readonly();
