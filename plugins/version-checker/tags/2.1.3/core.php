a220957b26cdd2a6d3549393ca179ac0e6a221eb
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-08-05 16:13:52 +0000

    Tag version-checker v.2.1.3
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic@4535 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ plugins/version-checker/tags/2.1.3/core.php
@@ -0,0 +1,262 @@
+<?php
+/**
+ * sem_update_core
+ *
+ * @package Version Checker
+ **/
+
+class sem_update_core {
+	/**
+	 * ob_start()
+	 *
+	 * @return void
+	 **/
+
+	function ob_start() {
+		if ( class_exists('WP_Nav_Menu_Widget') )
+			$update_core = get_site_transient('update_core');
+		else
+			$update_core = get_transient('update_core');
+		if ( !$update_core || empty($update_core->updates) )
+			return;
+		
+		echo '<div class="updated fade">' . "\n";
+		
+		echo '<p>'
+			. __('<strong>Important</strong>: Do not interrupt an upgrade once it\'s started. Doing so can potentially leave your site in a dysfunctional state. It can take several minutes to complete -- albeit seldom more than 15 minutes.', 'version-checker')
+			. '</p>' . "\n";
+
+		echo '<p>'
+			. sprintf(__('Also note that the Version Checker plugin proactively works around known issues in the WP Upgrade API. <a href="%1$s">There</a> <a href="%2$s">are</a> <a href="%3$s">some</a>, so please make sure you\'re using its latest version before proceeding (<a href="%4$s">change log</a>).', 'version-checker'),
+			'http://core.trac.wordpress.org/ticket/10140',
+			'http://core.trac.wordpress.org/ticket/10407',
+			'http://core.trac.wordpress.org/ticket/10541',
+			'http://www.semiologic.com/software/version-checker/')
+			. '</p>';
+		
+		echo '<p>'
+			. sprintf(__('Lastly, please note that it\'s <strong>much</strong> faster (and much safer) to upgrade WordPress from your hosting account\'s control panel than from this screen. Note that <strong><a href="%1$s">Hub users</a></strong> get to upgrade Semiologic packages as a bonus when they do so.', 'version-checker'), 'http://members.semiologic.com/hosting/')
+			. '</p>' . "\n";
+
+		echo '</div>' . "\n";
+		
+		global $action;
+		if ( !$_POST && $action == 'upgrade-core' ) {
+			global $mywpdbbackup;
+			if ( $mywpdbbackup ) {
+				remove_action('load-update-core.php', array(&$mywpdbbackup, 'update_notice_action'));
+				ob_start(array('sem_update_core', 'update_notice_action'));
+				add_action('admin_footer', array('sem_update_core', 'ob_flush'));
+			}
+			return;
+		}
+		
+		global $wp_version;
+		$bail = false;
+		
+		if ( $wp_version == '2.8' ) {
+			$file = file_get_contents(ABSPATH . 'wp-admin/includes/class-wp-upgrader.php');
+			$bail = strpos($file, '$wp_filesystem->delete($wp_dir,') !== false;
+		}
+		
+		if ( $bail ) {
+			echo '<div class="error">' . "\n";
+			
+			echo '<p><strong><big>'
+				. __('This automated upgrade has been cancelled because it can result in your site getting deleted.', 'version-checker')
+				. '</big></strong></p>' . "\n";
+			
+			echo '<p>'
+				. __('Your WordPress 2.8 installation has a critical bug (which is fixed in 2.8.1) whereby, if the automated upgrader fails while the files are being copied, the upgrader will <strong>delete</strong> all of your site\'s files (including your attachments, and other sites you may have in the same directory).', 'version-checker')
+				. '</p>' . "\n";
+			
+			echo '<p>'
+				. sprintf(__('Download and edit your site\'s <code>%s</code> file using notepad; find this line in the Core_Upgrader class (around lines 650-710):', 'version-checker'), 'wp-admin/includes/class-wp-upgrader.php')
+				. '</p>' . "\n";
+			
+			echo '<pre><code>$wp_filesystem->delete($wp_dir, true);</code></pre>' . "\n";
+			
+			echo '<p>'
+				. __('Change it to:', 'version-checker')
+				. '</p>' . "\n";
+			
+			echo '<pre><code>$wp_filesystem->delete($working_dir, true);</code></pre>' . "\n";
+			
+			echo '<p>'
+				. sprintf(__('Then, re-upload the file to your site and <a href="%s">start over</a>. You will no longer see this warning if it\'s safe to proceed.', 'version-checker'), 'update-core.php')
+				. '</p>' . "\n";
+			
+			echo '</div>' . "\n";
+			
+			version_checker::force_flush();
+			ob_start(array('sem_update_core', 'wp_2_8_ob_callback'));
+			add_action('in_footer', array('sem_update_core', 'ob_flush'), -1000);
+			
+			return;
+		} elseif ( !version_checker::check('sem-pro') ) {
+			return;
+		}
+		
+		# give some extra resources to WP
+		@ini_set('memory_limit', '256M');
+
+		echo '<div class="updated fade">' . "\n";
+
+		echo '<p>'
+			. __('As you await feedback from the WP upgrader, here are some preemptive troubleshooting tips in the event it fails...', 'version-checker')
+			. '</p>' . "\n";
+
+		echo '<p>'
+			. __('The single most common reason upgrades fail are server and network timeouts. They can have all sorts of origins, but the most frequent one (FTP timeouts) is related to your server\'s configuration and should be reported to your host. Before you do, however...', 'version-checker')
+			. '</p>' . "\n";
+
+		echo '<p>'
+			. __('WP 2.9 introduces an FS_TIMEOUT constant for FTP connections. Version Checker introduces the same in WP 2.8, and makes it default to 15 minutes (900 seconds). It\'s a garguantuan value by any measure -- but we found it necessary on slower hosts. If yours is even slower than slow, increase it by adding a define in your wp-config.php file, e.g.:', 'version-checker')
+			. '</p>' . "\n";
+
+		echo '<p><code>define(\'FS_TIMEOUT\', 1800); // 30 minutes</code></p>' . "\n";
+
+		echo '<p>'
+			. __('Now, on to the frequently encountered issues in case you get one of them... If the download is failing altogether, or if WP reports the zip is corrupt (a PCLZIP_ERR of some kind):', 'version-checker') . "\n"
+			. '</p>' . "\n";
+
+		echo '<ol>' . "\n";
+
+		echo '<li>'
+			. __('Start by trying again unless there is an obvious server configuration problem. There could have been a network problem, it could be that the originating server is getting hammered by a spike of downloads; it could be anything...', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. sprintf(__('If it continues to fail, activate the <a href="%s">Core Control</a> plugin, and enable the HTTP Access module under Tools / Upgrade.', 'version-checker'), 'http://dd32.id.au/wordpress-plugins/?plugin=core-control')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. __('Click &quot;External HTTP Access&quot; under Tools / Upgrade (near the screen\'s title), and disable the current HTTP transport.', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. __('Revisit this screen, and try again.', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. __('Repeat the above steps if it\'s still failing, until you run out of available transports to try. (Oftentimes, at least one of them will succeed.)', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. __('Don\'t forget to re-enable the HTTP transports if they all fail. (In this case, the odds are it\'s failing due to the way the server is configurated, and you should definitely report the issue to your host.)', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '</ol>' . "\n";
+
+		echo '<p>'
+			. __('If unzipping is failing (in this case, WP will typically report that it\'s failing to copy a file, always the same one, over and over):', 'version-checker') . "\n"
+			. '</p>' . "\n";
+
+		echo '<ol>' . "\n";
+
+		echo '<li>'
+			. __('Start by trying again. It might work this time.', 'version-checker')
+			. '</li>' . "\n";
+		
+		echo '<li>'
+			. __('If it fails again, consider emptying the wp-content/upgrade folder from your site using FTP software. It potentially contains folders that WP attempts to delete using poorly optimized code.', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. sprintf(__('If it continues to fail, activate the <a href="%s">Core Control</a> plugin, and enable the Filestem module under Tools / Core Control.', 'version-checker'), 'http://dd32.id.au/wordpress-plugins/?plugin=core-control')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. __('Click &quot;Filesystem Access&quot; under Tools / Core Control (near the screen\'s title), and switch to using the PHP FTP Sockets method if you are using the PHP FTP Extension. On some servers (though not all) the Sockets method works better than the built-in method.', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '</ol>' . "\n";
+
+		echo '<p>'
+			. __('If the file copying starts but bails at some point:', 'version-checker') . "\n"
+			. '</p>' . "\n";
+
+		echo '<ol>' . "\n";
+		
+		echo '<li>'
+			. __('Don\'t browse away from this screen. Especially not if a new WP version is involved. Your site might be unavailable due to an incomplete WP upgrade, and you want this screen to be around if this is the case. Load Tools / Upgrade in a new tab or window.', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. __('If you can, start by trying again. It might work this time.', 'version-checker')
+			. '</li>' . "\n";
+		
+		echo '<li>'
+			. __('If it fails again, consider emptying the wp-content/upgrade folder from your site using FTP software. It potentially contains folders that WP attempts to delete using poorly optimized code.', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. sprintf(__('If it continues to fail, activate the <a href="%s">Core Control</a> plugin, and enable the Filestem module under Tools / Core Control.', 'version-checker'), 'http://dd32.id.au/wordpress-plugins/?plugin=core-control')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. __('Click &quot;Filesystem Access&quot; under Tools / Core Control (near the screen\'s title), and switch to using the PHP FTP Sockets method if you are using the PHP FTP Extension. On some servers (though not all) the Sockets method works better than the built-in method.', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '<li>'
+			. sprintf(__('If it persists in failing, or if this screen is no longer available, you\'re in for a manual <a href="%1$s">install</a> or <a href="%2$s">upgrade</a>.', 'version-checker'), 'http://members.semiologic.com/sem-pro/install/', 'http://members.semiologic.com/sem-pro/upgrade/')
+			. '</li>' . "\n";
+		
+		echo '<li>'
+			. __('If you see a "there was a failed upgrade" sort of message in your admin area after a manual upgrade, it is due to the presence of a .maintenance file in your site\'s root folder. Delete it to remove it.', 'version-checker')
+			. '</li>' . "\n";
+
+		echo '</ol>' . "\n";
+		
+		echo '<p>'
+			. sprintf(__('And yes, it\'s <strong>much faster</strong> (and much safer) to upgrade from the control panel of your host. If you\'re considering a new host, seriously consider <strong><a href="%s">Hub</a></strong>.', 'version-checker'), 'http://members.semiologic.com/hosting/')
+			. '</p>' . "\n";
+		
+		echo '</div>' . "\n";
+		
+		version_checker::force_flush();
+	} # ob_start()
+	
+	
+	/**
+	 * ob_flush()
+	 *
+	 * @return void
+	 **/
+
+	function ob_flush() {
+		while ( ob_get_level() )
+			ob_end_flush();
+	} # ob_flush()
+	
+	
+	/**
+	 * wp_2_8_ob_callback()
+	 *
+	 * @param string $buffer
+	 * @return string $buffer
+	 **/
+
+	function wp_2_8_ob_callback($buffer) {
+		return preg_replace('/<form\b.+<\/form>/s', '', $buffer);
+	} # wp_2_8_ob_callback()
+	
+	
+	/**
+	 * update_notice_action()
+	 *
+	 * @param string $buffer
+	 * @return string $buffer
+	 **/
+
+	function update_notice_action($buffer) {
+		$buffer = str_replace(array(
+			__('http://codex.wordpress.org/WordPress_Backups'),
+			__('http://codex.wordpress.org/WordPress_Backups', 'version-checker'),
+			), 'tools.php?page=wp-db-backup', $buffer);
+		return $buffer;
+	} # update_notice_action()
+} # sem_update_core
+
+add_action('admin_notices', array('sem_update_core', 'ob_start'), 1000);
+?>
---
+++ plugins/version-checker/tags/2.1.3/plugins.php
@@ -0,0 +1,524 @@
+<?php
+/**
+ * sem_update_plugins
+ *
+ * @package Version Checker
+ **/
+
+class sem_update_plugins {
+	/**
+	 * install_plugins_tabs()
+	 *
+	 * @param array $tabs
+	 * @return array $tabs
+	 **/
+
+	function install_plugins_tabs($tabs) {
+		if ( get_site_option('sem_api_key') )
+			$tabs['semiologic'] = __('Semiologic', 'version-checker');
+		return $tabs;
+	} # install_plugins_tabs()
+	
+	
+	/**
+	 * install_plugins_semiologic()
+	 *
+	 * @param int $page
+	 * @return void
+	 **/
+
+	function install_plugins_semiologic($page = 1) {
+		if ( $_POST )
+			return;
+		
+		$args = array('browse' => 'semiologic', 'page' => $page);
+		$api = plugins_api('query_plugins', $args);
+		sem_update_plugins::display_plugins_table($api->plugins, $api->info['page'], $api->info['pages']);
+	} # install_plugins_semiologic()
+	
+	
+	/**
+	 * display_plugins_table()
+	 *
+	 * @param array $plugins
+	 * @param int $page
+	 * @param int $pages
+	 * @return void
+	 **/
+
+	function display_plugins_table($plugins, $page = 1, $pages = 1) {
+		$sem_plugins = sem_update_plugins::cache();
+		
+		if ( !$sem_plugins ) {
+			echo '<p>'
+				. __('Plugin lookup failed. Please refresh this page in a few minutes to try again.', 'version-checker')
+				. '</p>' . "\n";
+			return;
+		}
+		
+		$to_install = array();
+		$to_upgrade = array();
+		
+		$installed = get_plugins();
+		if ( class_exists('WP_Nav_Menu_Widget') )
+			$response = get_site_transient('update_plugins');
+		else
+			$response = get_transient('update_plugins');
+		$response = is_object($response) ? (array) $response->response : array();
+		
+		foreach ( array_keys($sem_plugins) as $slug ) {
+			$file1 = "$slug/$slug.php";
+			$file2 = "$slug.php";
+			if ( !isset($installed[$file1]) && !isset($installed[$file2]) && $sem_plugins[$slug]->download_link )
+				$to_install[] = $slug;
+		}
+		
+		foreach ( $response as $file => $resp ) {
+			if ( version_compare($response[$file]->new_version, $installed[$file]['Version'], '>') && $response[$file]->package )
+				$to_upgrade[] = $resp->slug;
+		}
+		
+		$type = isset($_REQUEST['type']) ? stripslashes( $_REQUEST['type'] ) : '';
+		$term = isset($_REQUEST['s']) ? stripslashes( $_REQUEST['s'] ) : '';
+	
+		$plugins_allowedtags = array('a' => array('href' => array(),'title' => array(), 'target' => array()),
+									'abbr' => array('title' => array()),'acronym' => array('title' => array()),
+									'code' => array(), 'pre' => array(), 'em' => array(),'strong' => array(),
+									'ul' => array(), 'ol' => array(), 'li' => array(), 'p' => array(), 'br' => array());
+
+?>
+	<div class="tablenav">
+		<div class="alignleft actions">
+		<?php
+		if ( $to_install ) {
+			echo '<form method="post" action="" style="display: inline;">' . "\n"
+				. '<input type="submit" class="button" value="' . esc_attr(sprintf(__('Mass Install (%s)', 'version-checker'), count($to_install))) . '" />'
+				. '<input type="hidden" name="action" value="mass-install" />' . "\n";
+			wp_nonce_field('mass-install');
+			echo '</form>' . "\n";
+		}
+		if ( $to_upgrade ) {
+			echo '<form method="post" action="" style="display: inline;">' . "\n"
+				. '<input type="submit" class="button" value="' . esc_attr(sprintf(__('Mass Upgrade (%s)', 'version-checker'), count($to_upgrade))) . '" />'
+				. '<input type="hidden" name="action" value="mass-upgrade" />' . "\n";
+			wp_nonce_field('mass-upgrade');
+			echo '</form>' . "\n";
+		}
+		?>
+		</div>
+		<?php
+			$url = esc_url($_SERVER['REQUEST_URI']);
+			if ( ! empty($term) )
+				$url = add_query_arg('s', $term, $url);
+			if ( ! empty($type) )
+				$url = add_query_arg('type', $type, $url);
+			
+			$totalpages = 1;
+			$page_links = paginate_links( array(
+				'base' => add_query_arg('paged', '%#%', $url),
+				'format' => '',
+				'prev_text' => __('&laquo;', 'version-checker'),
+				'next_text' => __('&raquo;', 'version-checker'),
+				'total' => $totalpages,
+				'current' => $page
+			));
+
+			if ( $page_links )
+				echo "\t\t<div class='tablenav-pages'>$page_links</div>";
+?>
+		<br class="clear" />
+	</div>
+	<table class="widefat" id="install-plugins" cellspacing="0">
+		<thead>
+			<tr>
+				<th scope="col" class="name"><?php _e('Name', 'version-checker'); ?></th>
+				<th scope="col" class="num"><?php _e('Version', 'version-checker'); ?></th>
+				<th scope="col" class="desc"><?php _e('Description', 'version-checker'); ?></th>
+				<th scope="col" class="action-links"><?php _e('Actions', 'version-checker'); ?></th>
+			</tr>
+		</thead>
+
+		<tfoot>
+			<tr>
+				<th scope="col" class="name"><?php _e('Name', 'version-checker'); ?></th>
+				<th scope="col" class="num"><?php _e('Version', 'version-checker'); ?></th>
+				<th scope="col" class="desc"><?php _e('Description', 'version-checker'); ?></th>
+				<th scope="col" class="action-links"><?php _e('Actions', 'version-checker'); ?></th>
+			</tr>
+		</tfoot>
+
+		<tbody class="plugins">
+		<?php
+			if( empty($plugins) )
+				echo '<tr><td colspan="5">', __('No plugins match your request.', 'version-checker'), '</td></tr>';
+
+			foreach( (array) $plugins as $plugin ){
+				if ( is_object($plugin) )
+					$plugin = (array) $plugin;
+				
+				$title = wp_kses($plugin['name'], $plugins_allowedtags);
+				//Limit description to 400char, and remove any HTML.
+				$description = strip_tags($plugin['description']);
+				if ( strlen($description) > 400 )
+					$description = mb_substr($description, 0, 400) . '&#8230;';
+				//remove any trailing entities
+				$description = preg_replace('/&[^;\s]{0,6}$/', '', $description);
+				//strip leading/trailing & multiple consecutive lines
+				$description = trim($description);
+				$description = preg_replace("|(\r?\n)+|", "\n", $description);
+				//\n => <br>
+				$description = nl2br($description);
+				$version = wp_kses($plugin['version'], $plugins_allowedtags);
+
+				$name = strip_tags($title . ' ' . $version);
+
+				$author = '';
+				if( ! empty($plugin['author']) )
+					$author = ' <cite>' . sprintf( __('By %s', 'version-checker'), $plugin['author'] ) . '.</cite>';
+
+				$author = wp_kses($author, $plugins_allowedtags);
+
+				if( isset($plugin['homepage']) )
+					$title = '<a target="_blank" href="' . esc_attr($plugin['homepage']) . '">' . $title . '</a>';
+
+				$action_links = array();
+				if ( in_array($plugin['slug'], $to_install) ) {
+					$action_links[] = '<a href="' . admin_url('plugin-install.php?tab=plugin-information&amp;plugin=' . $plugin['slug'] .
+										'&amp;TB_iframe=true&amp;width=600&amp;height=550') . '" class="thickbox onclick" title="' .
+										esc_attr($name) . '">' . __('Install', 'version-checker') . '</a>';
+				} elseif ( in_array($plugin['slug'], $to_upgrade) ) {
+					$action_links[] = '<a href="' . admin_url('plugin-install.php?tab=plugin-information&amp;plugin=' . $plugin['slug'] .
+										'&amp;TB_iframe=true&amp;width=600&amp;height=550') . '" class="thickbox onclick" title="' .
+										esc_attr($name) . '">' . __('Upgrade', 'version-checker') . '</a>';
+				} elseif ( !$sem_plugins[$plugin['slug']]->download_link ) {
+					$action_links[] = '<strong><a href="http://oldbackend.semiologic.com/order.php">'
+						. __('Order', 'version-checker')
+						. '</a></strong>';
+				}
+			?>
+			<tr>
+				<td class="name"><?php echo $title; ?></td>
+				<td class="vers"><?php echo $version; ?></td>
+				<td class="desc"><?php echo $description, $author; ?></td>
+				<td class="action-links"><?php if ( !empty($action_links) )	echo implode(' | ', $action_links); ?></td>
+			</tr>
+			<?php
+			}
+			?>
+		</tbody>
+	</table>
+
+	<div class="tablenav">
+		<?php if ( $page_links )
+				echo "\t\t<div class='tablenav-pages'>$page_links</div>"; ?>
+		<br class="clear" />
+	</div>
+
+<?php
+	} # display_plugins_table()
+	
+	
+	/**
+	 * mass_install()
+	 *
+	 * @param array $todo
+	 * @return void
+	 **/
+
+	function mass_install($plugins) {
+		include_once dirname(__FILE__) . '/upgrader.php';
+		
+		$url = 'tools.php?page=sem-tools&amp;action=' . urlencode($_REQUEST['action']);
+		$title = __('Install Plugins', 'version-checker');
+		$nonce = 'mass-install';
+		$upgrader = new sem_upgrader( new sem_installer_skin( compact('title', 'nonce', 'url', 'plugin') ) );
+		$upgrader->bulk_install($plugins);
+	} # mass_install()
+	
+	
+	/**
+	 * mass_upgrade()
+	 *
+	 * @param array $todo
+	 * @return void
+	 **/
+
+	function mass_upgrade($plugins) {
+		include_once dirname(__FILE__) . '/upgrader.php';
+		
+		$url = 'tools.php?page=sem-tools&amp;action=' . urlencode($_REQUEST['action']);
+		$title = __('Upgrade Plugins', 'version-checker');
+		$nonce = 'mass-upgrade';
+		$upgrader = new sem_upgrader( new sem_upgrader_skin( compact('title', 'nonce', 'url', 'plugin') ) );
+		$upgrader->bulk_upgrade($plugins);
+	} # mass_upgrade()
+	
+	
+	/**
+	 * plugins_api()
+	 *
+	 * @param false $res
+	 * @param string $action
+	 * @param array $args
+	 * @return $res
+	 **/
+
+	function plugins_api($res, $action, $args) {
+		if ( $res || !get_site_option('sem_api_key') )
+			return $res;
+		
+		switch ( $action ) {
+		case 'plugin_information':
+			return sem_update_plugins::info($res, $action, $args);
+		
+		case 'query_plugins':
+		if ( !empty($args->browse) && $args->browse == 'semiologic' )
+			return sem_update_plugins::query($res, $action, $args);
+		
+		default:
+			return $res;
+		}
+	} # plugins_api()
+	
+	
+	/**
+	 * query()
+	 *
+	 * @param false $res
+	 * @param string $action
+	 * @param array $args
+	 * @return mixed $res
+	 **/
+
+	function query($res, $action, $args) {
+		$res = (object) array(
+			'info' => array('page' => 1, 'pages' => 1, 'results' => 0),
+			'plugins' => array(),
+			);
+		
+		$response = sem_update_plugins::cache();
+		if ( $response && is_array($response) ) {
+			$res->info['results'] = count($response);
+			$res->plugins = $response;
+			usort($res->plugins, array('sem_update_plugins', 'sort'));
+		}
+		
+		return $res;
+	} # query()
+	
+	
+	/**
+	 * info()
+	 *
+	 * @param false $res
+	 * @param string $action
+	 * @param array $args
+	 * @return mixed $res
+	 **/
+
+	function info($res, $action, $args) {
+		$plugins = sem_update_plugins::cache();
+		if ( !isset($plugins[$args->slug]) || empty($plugins[$args->slug]->download_link) )
+			return $res;
+		
+		if ( !preg_match("!^https?://[^/]+.semiologic.com!", $plugins[$args->slug]->download_link) )
+			return $res;
+		
+		return $plugins[$args->slug];
+	} # info()
+	
+	
+	/**
+	 * sort()
+	 *
+	 * @param object $a
+	 * @param object $b
+	 * @return int $sort
+	 **/
+
+	function sort($a, $b) {
+		return strnatcmp($a->name, $b->name);
+	} # sort()
+	
+	
+	/**
+	 * cache()
+	 *
+	 * @param string $type
+	 * @return array $plugins
+	 **/
+
+	function cache() {
+		if ( class_exists('WP_Nav_Menu_Widget') )
+			$response = get_site_transient('sem_query_plugins');
+		else
+			$response = get_transient('sem_query_plugins');
+		if ( $response !== false )
+			return $response;
+		
+		global $wp_version;
+		$sem_api_key = get_site_option('sem_api_key');
+		
+		$url = sem_api_info . '/plugins/' . $sem_api_key;
+		
+		$body = array(
+			'action' => 'query',
+			'packages' => get_site_option('sem_packages'),
+			);
+		
+		$options = array(
+			'timeout' => 15,
+			'body' => $body,
+			'user-agent' => 'WordPress/' . preg_replace("/\s.*/", '', $wp_version) . '; ' . get_bloginfo('url'),
+			);
+		
+		$cache_id = md5(serialize(array($url, $options)));
+		$raw_response = wp_cache_get($cache_id, 'sem_api');
+		if ( $raw_response === false ) {
+			$raw_response = wp_remote_post($url, $options);
+			wp_cache_set($cache_id, $raw_response, 'sem_api');
+		}
+		
+		if ( is_wp_error($raw_response) || 200 != $raw_response['response']['code'] )
+			$response = false;
+		else
+			$response = @unserialize($raw_response['body']);
+		
+		if ( $response !== false ) {
+			$response = sem_update_plugins::parse($response);
+			if ( class_exists('WP_Nav_Menu_Widget') )
+				set_site_transient('sem_query_plugins', $response, 7200);
+			else
+				set_transient('sem_query_plugins', $response, 7200);
+		}
+		
+		return $response;
+	} # cache()
+	
+	
+	/**
+	 * parse()
+	 *
+	 * @param object $obj
+	 * @return object $obj
+	 **/
+
+	function parse($obj) {
+		if ( is_array($obj) ) {
+			$res = array();
+			foreach ( $obj as $k => $v ) {
+				$v = sem_update_plugins::parse($v);
+				if ( $v && is_object($v) && $v->slug )
+					$res[$v->slug] = $v;
+			}
+			ksort($res);
+			$obj = null;
+			return $res;
+		} elseif ( !is_object($obj) ) {
+			return false;
+		}
+		
+		if ( empty($obj->readme) || !trim($obj->readme) )
+			return false;
+		
+		if ( !function_exists('Markdown') )
+			include_once dirname(__FILE__) . '/markdown/markdown.php';
+		global $allowedposttags;
+		$plugins_allowedtags = array('a' => array('href' => array(),'title' => array()),'abbr' => array('title' => array()),'acronym' => array('title' => array()),'code' => array(),'em' => array(),'strong' => array());
+		
+		$readme = str_replace(array("\r\n", "\r"), "\n", $obj->readme);
+		$readme = preg_split("/^\s*(==[^=].+?)\s*$/m", $readme, null, PREG_SPLIT_DELIM_CAPTURE);
+		
+		$header = array_shift($readme);
+		$header = explode("\n", $header);
+		
+		$name = false;
+		do {
+			$name = array_shift($header);
+			if ( preg_match("/^===\s*(.+?)\s*(?:===)?$/", $name, $name) )
+				$obj->name = end($name);
+		} while ( $header && !$obj->name );
+		
+		while ( $field = array_shift($header) ) {
+			if ( preg_match("/\s*Contributors\s*:\s*(.*)/i", $field, $author) )
+				$obj->author = end($author);
+		}
+		
+		$obj->description = wp_kses(Markdown(implode("\n", $header)), $allowedposttags);
+		$obj->sections = array();
+		
+		if ( $readme ) {
+			do {
+				$section = array_shift($readme);
+				$section = trim($section);
+
+				if ( preg_match("/^==\s*(Description)\s*(?:==)?$/i", $section, $desc) ) {
+					$desc = str_replace('-', '_', sanitize_title(end($desc)));
+					$section = array_shift($readme);
+					$section = preg_replace("/^=([^=].+?)=?$/m", "### $1", $section);
+					$section = markdown($section);
+					$section = wp_kses($section, $allowedposttags);
+					$obj->sections[$desc] = $section;
+				} elseif ( preg_match("/^==\s*(Change\s*Log)\s*(?:==)?$/i", $section, $desc) ) {
+					$desc = str_replace('-', '_', sanitize_title(end($desc)));
+					$section = array_shift($readme);
+					$section = preg_replace("/^=([^=].+?)=?$/m", "### " . sprintf(__('Version %s', 'version-checker'), "$1"), $section);
+					$section = markdown($section);
+					$section = wp_kses($section, $allowedposttags);
+					$obj->sections[$desc] = $section;
+				}
+			} while ( $readme );
+		}
+		
+		# sanitize
+		$obj->compatability = array();
+		
+		foreach ( get_object_vars($obj) as $key => $val ) {
+			switch ( $key ) {
+			case 'name':
+			case 'slug':
+			case 'version':
+			case 'requires':
+			case 'tested':
+			case 'last_updated':
+				$obj->$key = wp_kses($val, $plugins_allowedtags);
+				break;
+			
+			case 'rating':
+			case 'num_ratings':
+				$obj->$key = intval($val);
+				break;
+			
+			case 'homepage':
+			case 'download_link':
+				$obj->$key = clean_url($val);
+				break;
+			
+			case 'author':
+				$url = clean_url($obj->homepage);
+				if ( preg_match("!^https?://[^/]+.semiologic.com!", $url) )
+					$url = 'http://www.semiologic.com';
+				$obj->$key = '<a href="' . $url . '">'
+					. str_replace('-', ' ', wp_kses($val, $plugins_allowedtags))
+					. '</a>';
+				break;
+			
+			case 'description':
+			case 'sections':
+				break; # already sanitized
+			
+			default:
+				unset($obj->$key);
+				break;
+			}
+		}
+		
+		return $obj;
+	} # parse()
+} # sem_update_plugins
+
+add_filter('install_plugins_tabs', array('sem_update_plugins', 'install_plugins_tabs'));
+add_action('install_plugins_semiologic', array('sem_update_plugins', 'install_plugins_semiologic'));
+
+add_filter('plugins_api', array('sem_update_plugins', 'plugins_api'), 10, 3);
+?>
